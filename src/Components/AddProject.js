2e9b514aa54c5296dd9d5a710522e3f5b2eb6b92
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 11:50:18 +0200

    added select and input for category add

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class AddProject extends Component {
+    static defaultProps = {
+        categories: ['Web Design', 'Web Dev', 'Mobile Dev']
+    }
     render() {
+        let categoryOptions = this.props.cateogries.map(category => {
+            return <option key={category} value="category">{category}</option> 
+        });
+        
         return (
             <div>
                 <h3>Add Project</h3>
+                <form>
+                    <div>
+                        <label>Title</label><br />
+                        <input type="text" ref="title"/>
+                    </div>
+                    <div>
+                        <label>Category</label><br />
+                        <select ref="category">
+                            {categoryOptions}
+                        </select>
+                    </div>
+                </form>
             </div>
         );
     }
