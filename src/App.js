d0bee2b1dc69d9344915049e4f4126fb1d2fd7a6
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 11:38:30 +0200

    projectsitem

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            projects: [
+                {
+                    title: 'Business Website',
+                    category: 'Web Design'
+                },
+                {
+                    title: 'Social App',
+                    category: 'Mobile Dev'
+                },
+                {
+                    title: 'Ecommerce Shopping Cart',
+                    category: 'Web Dev'
+                }
+            ]
+        }
+    }
+    
     render() {
         return (
             <div className="App">
-                <Projects />
+                <Projects projects={this.state.projects}/>
             </div>
         );
     }
--- src/Components/ProjectsItem.js
+++ src/Components/ProjectsItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Projects extends Component {
+class ProjectsItem extends Component {
     render() {
         return (
             <div className="Projects">
@@ -10,4 +10,4 @@ class Projects extends Component {
     }
 }
 
-export default Projects;
+export default ProjectsItem;
