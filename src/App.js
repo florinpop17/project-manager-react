7677f155b390d1b81d7749273cec807799233192
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 11:31:23 +0200

    added projects component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Projects />
             </div>
         );
     }
