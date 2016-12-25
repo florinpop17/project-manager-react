014619b65238e40155e95b6b912fe91d77cf1bf1
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 11:28:49 +0200

    removed component html

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <h1>Very cool</h1>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
 }
 
 export default App;
