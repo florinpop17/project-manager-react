67e4c4735ea80aa7b39c133f74703ba9438582f8
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 11:31:25 +0200

    added projects component

---
+++ src/Components/Projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
