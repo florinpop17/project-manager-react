3f63864decd285eee979609575d8fecd550d2fdf
Florin Pop <popflorin1705@yahoo.com>
2016-12-25 12:13:52 +0200

    added form submit

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -4,15 +4,22 @@ class AddProject extends Component {
     static defaultProps = {
         categories: ['Web Design', 'Web Dev', 'Mobile Dev']
     }
+    
+    handleSubmit(e) {
+        console.log('submitted');
+        
+        e.preventDefault();
+    }
+    
     render() {
-        let categoryOptions = this.props.cateogries.map(category => {
+        let categoryOptions = this.props.categories.map(category => {
             return <option key={category} value="category">{category}</option> 
         });
         
         return (
             <div>
                 <h3>Add Project</h3>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <label>Title</label><br />
                         <input type="text" ref="title"/>
@@ -23,6 +30,7 @@ class AddProject extends Component {
                             {categoryOptions}
                         </select>
                     </div>
+                    <input type="submit" value="submit"/>
                 </form>
             </div>
         );
